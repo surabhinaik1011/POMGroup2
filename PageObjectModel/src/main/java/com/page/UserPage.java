2241f02311685b53d5d0516346838e7ef8eb1f17
Mayurtalan <mayurtalan911@gmail.com>
2020-07-08 19:23:08 +0530

    Add user By Mayur

--- PageObjectModel/src/main/java/com/page/UserPage.java
+++ PageObjectModel/src/main/java/com/page/UserPage.java
@@ -1,5 +1,77 @@
 package com.page;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
 public class UserPage {
+
+	WebDriver driver;
+	List<WebElement> cityData;
+
+	public UserPage(WebDriver driver) {
+		this.driver = driver;
+	}
+
+	private By adduser = By.xpath("//a[contains(@href,'add_user.html')] //button");
+	private By userName = By.xpath("//input[@placeholder='Username']");
+	private By mobile = By.id("mobile");
+	private By email = By.id("email");
+	private By course = By.xpath("//input[@placeholder='Java/J2EE']");
+	private By gender = By.id("Male");
+	private By SelectSate = By.xpath("//div[@class='form-group']//select");
+	private By city = By.xpath("//select //option");
+	private By password = By.id("password");
+	private By submit = By.id("submit");
+	private By Cancel = By.xpath("//div[@class='box-footer']//span[1]");
+
+	public WebElement getAddUser(WebDriver driver) {
+		return driver.findElement(adduser);
+	}
+
+	public WebElement getuserName(WebDriver driver) {
+		return driver.findElement(userName);
+	}
+
+	public WebElement getmobile(WebDriver driver) {
+		return driver.findElement(mobile);
+	}
+
+	public WebElement getemail(WebDriver driver) {
+		return driver.findElement(email);
+	}
+
+	public WebElement getcourse(WebDriver driver) {
+		return driver.findElement(course);
+	}
+
+	public WebElement getgender(WebDriver driver) {
+		return driver.findElement(gender);
+	}
+
+	public WebElement getSelectState(WebDriver driver) {
+		return driver.findElement(SelectSate);
+	}
+
+	public WebElement getpassword(WebDriver driver) {
+		return driver.findElement(password);
+	}
+
+	public List<WebElement> getcity(WebDriver driver) {
+		cityData = driver.findElements(city);
+		return cityData;
+
+	}
+
+	public WebElement getsubmit(WebDriver driver) {
+		return driver.findElement(submit);
+	}
+
+	public WebElement getCancel(WebDriver driver) {
+		return driver.findElement(Cancel);
+	}
 
 }
--- PageObjectModel/src/test/java/com/test/UserTest.java
+++ PageObjectModel/src/test/java/com/test/UserTest.java
@@ -1,5 +1,76 @@
 package com.test;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.List;
+import java.util.Properties;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.page.UserPage;
+
 public class UserTest {
 
+	public WebDriver driver;
+
+	@BeforeClass
+	public WebDriver getDriver() throws IOException {
+
+		System.setProperty("webdriver.chrome.driver", "D:\\Testing Projects\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("file:///D:/website/Offline%20Website/index.html");
+
+		return driver;
+	}
+
+	@Test()
+	public void navigateToDashboard() {
+		driver.findElement(By.id("email")).sendKeys("kiran@gmail.com");
+		driver.findElement(By.id("password")).sendKeys("123456");
+		driver.findElement(By.xpath("//button[text()='Sign In']")).click();
+	}
+
+	@Test(priority = 1)
+	public void clickOnUser() {
+		driver.findElement(By.xpath("//a[contains(@href,'users.html')]")).click();
+	}
+
+	@Test(priority = 2)
+	public void clickAddUser() throws InterruptedException {
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("//a[contains(@href,'add_user.html')] //button")).click();
+	}
+
+	@Test(priority = 3)
+	public void addUserTest() throws InterruptedException, IOException {
+		UserPage up = new UserPage(driver);
+
+		Thread.sleep(1000);
+		up.getuserName(driver).sendKeys("Mayur Talan");
+		up.getmobile(driver).sendKeys("00000000");
+		up.getemail(driver).sendKeys("mayurtalan@gmail.com");
+		up.getcourse(driver).sendKeys("Selenium");
+		up.getgender(driver).click();
+		up.getSelectState(driver).click();
+		List<WebElement> cityData = up.getcity(driver);
+		System.out.println(cityData.size());
+
+		for (int i = 0; i < cityData.size() - 1; i++) {
+			if (cityData.get(i).getText().equalsIgnoreCase("HP")) {
+				Thread.sleep(2000);
+				cityData.get(i).click();
+				break;
+			}
+		}
+
+		System.out.println(cityData.size());
+		up.getpassword(driver).sendKeys("12345");
+		up.getsubmit(driver).click();
+
+	}
 }
