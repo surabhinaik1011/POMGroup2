4f221ea966a0c5d1a0d4d21b7ff7ef4ba6d32623
ketakichalse19 <ketakichalse24@gmail.com>
2020-07-08 14:07:06 +0000

    Registration Page

--- PageObjectModel/src/main/java/com/page/RegistrationPage.java
+++ PageObjectModel/src/main/java/com/page/RegistrationPage.java
@@ -1,11 +1,62 @@
 package com.page;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class RegistrationPage {
+	WebDriver driver;
 
-	public RegistrationPage(WebDriver driver) {
-		// TODO Auto-generated constructor stub
+	@FindBy(linkText= "Register a new membership")
+	private WebElement registerMember;
+	
+	@FindBy(id= "name")
+	private WebElement name;
+	
+	@FindBy(id= "mobile")
+	private WebElement mobile;
+	
+	@FindBy(id= "email")
+	private WebElement email;
+	
+	@FindBy(id= "password")
+	private WebElement password;
+	
+	@FindBy(xpath= "//button")
+	private WebElement signinButton;
+	
+	@FindBy(xpath= "//b")
+	private WebElement heading;
+	
+	public RegistrationPage(WebDriver driver)
+	{
+		PageFactory.initElements(driver, this);
 	}
-
+	
+	public void ClickToRegister()
+	{
+		registerMember.click();
+	}
+	public void EnterName(String uname)
+	{
+		name.sendKeys(uname);
+	}
+	public void EnterMobile(String mob)
+	{
+		mobile.sendKeys(mob);		
+	}
+	public void EnterEmail(String eml)
+	{
+		email.sendKeys(eml);
+	}
+	public void EnterPassword(String pass)
+	{
+		password.sendKeys(pass);
+	}
+	public void ClickToSignIn()
+	{
+		signinButton.click();
+	}
+		
 }
--- PageObjectModel/src/test/java/com/test/RegistrationTest.java
+++ PageObjectModel/src/test/java/com/test/RegistrationTest.java
@@ -1,5 +1,50 @@
 package com.test;
 
-public class RegistrationTest {
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
 
+import com.page.RegistrationPage;
+
+public class RegistrationTest {
+	WebDriver driver;
+	RegistrationPage register;
+	@BeforeSuite 
+ 	public void LaunchApplication()
+ 	{	
+	 System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+	 driver = new ChromeDriver(); 
+	 driver.get("file:///C:/Users/user/Desktop/Offline%20Website/Offline%20Website/index.html");
+	 driver.manage().window().maximize();
+ 	}
+ 	
+	@Test(priority = 1)
+	public void RegisterMember()
+	{
+	RegistrationPage register = new RegistrationPage(driver);
+    register.ClickToRegister();
+	register.EnterName("ketaki");
+	register.EnterMobile("7684939842");
+	register.EnterEmail("ketaki@gmail.com");
+	register.EnterPassword("12345");
+	register.ClickToSignIn();	
+	}
+	
+    @Test(priority = 2)
+    public void HandleAlert() throws Throwable
+    {
+    	Alert al = driver.switchTo().alert();
+		al.accept();
+    }
+	
+	@AfterSuite
+	public void CloseLaunchApplication(){
+		driver.close();
+	}
+	
 }
+
+
